69afa4225ee3609f1de1ef4cf605f1fa0ca0c58d
lorena_olay <olaylorena2002@gmail.com>
2021-11-26 14:41:23 +0100

    ffdfda

--- src/main/java/daw/poo/PRBOBARCANCION.java
+++ src/main/java/daw/poo/PRBOBARCANCION.java
@@ -2,6 +2,10 @@ package daw.poo;
 
 public class PRBOBARCANCION {
     public static void main(String[] args) {
-
+    CANCION fall;
+    CANCION hatersgonnahate;
+    fall = new CANCION("Falling","HARRY STYLE");
+    hatersgonnahate = new CANCION("22","TAYLOR QUEEN");
+        System.out.println("NOMINADOS: " + fall.getAutor() + ", " + hatersgonnahate.getAutor());
     }
 }
